6c56b691d185a8c203e0fe7bff27d8eac5fca11e
asadbekme <asadbekme2002@gmail.com>
2023-07-25 23:50:27 +0500

    removePhone method

--- phones-app/src/App.jsx
+++ phones-app/src/App.jsx
@@ -10,6 +10,14 @@ class App extends Component {
     this.setState({ phones: phones });
   }
 
+  removePhone = (id) => {
+    const { phones } = this.state;
+    phones.splice(id, 1);
+    // const filteredPhones = phones.filter((phone) => phone.id !== id);
+    // console.log(filteredPhones);
+    this.setState({ phones });
+  };
+
   render() {
     return (
       <div className="phones">
@@ -26,14 +34,15 @@ class App extends Component {
                     <th>Rating</th>
                     <th>Brand</th>
                     <th>Image</th>
+                    <th>Actions</th>
                   </tr>
                 </thead>
                 <tbody>
                   {phones.map((phone, index) => (
-                    <tr key={index + 1}>
+                    <tr key={phone.id}>
                       <td>{phone.id}</td>
                       <td>{phone.title}</td>
-                      <td>{phone.price}</td>
+                      <td>${phone.price}</td>
                       <td>{phone.rating}</td>
                       <td>{phone.brand}</td>
                       <td>
@@ -42,6 +51,14 @@ class App extends Component {
                           src={phone.thumbnail}
                           alt="Phone image"
                         />
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => this.removePhone(index)}
+                          className="btn btn-danger btn-sm"
+                        >
+                          Remove
+                        </button>
                       </td>
                     </tr>
                   ))}
